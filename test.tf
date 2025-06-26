096e9e07d4ee511d7daf013eb19b4ffbcf54ce97
shimoji <shimoji@linus.co.jp>
2025-06-26 17:00:41 +0900

    resourceの追加

--- test.tf
+++ test.tf
@@ -15,3 +15,33 @@ resource "aws_subnet" "dougbako_subnet" {
     Name = "dougbako_subnet"
   }
 }
+
+resource "aws_internet_gateway" "dougbako_gw" {
+  vpc_id = aws_vpc.dougbako_vpc.id
+
+  tags = {
+    Name = "dougbako_gw"
+  }
+}
+
+resource "aws_route_table" "dougbako_route_table" {
+  vpc_id = aws_vpc.dougbako_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dougbako_gw.id
+  }
+
+  tags = {
+    Name = "dougbako_route_table"
+  }
+}
+resource "aws_main_route_table_association" "dougbako_vpc_attache_a" {
+  vpc_id         = aws_vpc.dougbako_vpc.id
+  route_table_id = aws_route_table.dougbako_route_table.id
+}
+resource "aws_route_table_association" "dougbako_subnet_attache_a" {
+  subnet_id      = aws_subnet.dougbako_subnet.id
+  route_table_id = aws_route_table.dougbako_route_table.id
+}
+
