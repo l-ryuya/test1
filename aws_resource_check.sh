2ac66d11d41f67fbae97377f779866f8885f314c
shimoji <shimoji@linus.co.jp>
2025-06-26 16:36:27 +0900

    subnetチェック

--- aws_resource_check.sh
+++ aws_resource_check.sh
@@ -6,3 +6,11 @@ VPC_ID=$(aws ec2 describe-vpcs --filter "Name=tag:Name,Values=dougbako_vpc" --qu
   else
     echo "VPCがないので作成します"
   fi
+
+SUBNET_ID=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=dougbako_subnet" --query "Subnets[0].SubnetId" --output text 2> /dev/null)
+  if [ "$VPC_ID" != "None" ]; then
+    echo "既存のVPC($VPC_ID)をimportします"
+    terraform import aws_vpc.dougbako_subnet $SUBNET_ID
+  else
+    echo "VPCがないので作成します"
+  fi
