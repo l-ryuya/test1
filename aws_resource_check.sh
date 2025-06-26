48fe053fa322eae8c4a026f42243be6e798d97ee
shimoji <shimoji@linus.co.jp>
2025-06-26 16:17:49 +0900

    修正

--- aws_resource_check.sh
+++ aws_resource_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VPC_ID=$(aws ec2 describe-vpcs --filter "Name=tag:Name,Values=dougbako" --query "Vpcs[0].VpcId" --output text /dev/null 2>&1)
+VPC_ID=$(aws ec2 describe-vpcs --filter "Name=tag:Name,Values=dougbako" --query "Vpcs[0].VpcId" --output text 2> /dev/null)
   if [ "$VPC_ID" != "None" ]; then
     echo "既存のVPC($VPC_ID)をimportします"
     terraform import aws_vpc.dougbako_vpc $VPC_ID
