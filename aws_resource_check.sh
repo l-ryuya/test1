04f6edf6f7e2ba7b7e52a511389095e2ab15da92
shimoji <shimoji@linus.co.jp>
2025-06-26 16:11:59 +0900

    チェックをスクリプトで行う

---
+++ aws_resource_check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+VPC_ID=$(aws ec2 describe-vpcs --filter "Name=tag:Name,Values=dougbako" --query "Vpcs[0].VpcId" --output text /dev/null 2>&1)
+  if [ "$VPC_ID" != "None" ]; then
+    echo "既存のVPC($VPC_ID)をimportします"
+    terraform import aws_vpc.dougbako_vpc $VPC_ID
+  else
+    echo "VPCがないので作成します"
+  fi
