4a6bedafe4c0e0121c503113b1bb770afd00049e
shimoji <shimoji@linus.co.jp>
2025-06-26 16:26:21 +0900

    subnet追加

--- test.tf
+++ test.tf
@@ -1,8 +1,17 @@
 resource "aws_vpc" "dougbako_vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/24"
   instance_tenancy = "default"
 
   tags = {
-    Name = "dougbako"
+    Name = "dougbako_vpc"
+  }
+}
+
+resource "aws_subnet" "in_secondary_cidr" {
+  vpc_id     = aws_vpc.dougbako_vpc.vpc_id
+  cidr_block = "10.0.0.0/26"
+
+  tags = {
+    Name = "dougbako_subnet"
   }
 }
