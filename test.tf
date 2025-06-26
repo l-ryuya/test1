3469b9cebf6bdd2d08f9fcd9a51b64af69185d8a
shimoji <shimoji@linus.co.jp>
2025-06-26 17:35:41 +0900

    修正

--- test.tf
+++ test.tf
@@ -33,7 +33,7 @@ data "aws_route_table" "dougbako_main_route_table" {
 }
 
 resource "aws_route" "internet_access" {
-  route_table_id         = data.aws_route_tables.dougbako_main_route_table.id
+  route_table_id         = data.aws_route_table.dougbako_main_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.dougbako_gw.id
 }
